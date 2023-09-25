17ada30a46b4317befc4abe05ce709fab789bec1
João Tribuzy <joaovtribuzy@gmail.com>
2023-09-25 16:42:21 -0400

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by João Tribuzy on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
